c0005a9fd507b7fdd79d18355039d6232fe48153
soohocoding <ckdgns1447@gmail.com>
2023-11-09 15:04:01 +0900

    프런트 컨트롤러 패턴

---
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -0,0 +1,43 @@
+package org.example.mvc;
+
+import org.example.mvc.controller.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+
+private static Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+
+private RequestMappingHandlerMaping rehm;
+
+    @Override
+    public void init() throws ServletException {
+        rehm = new RequestMappingHandlerMaping();
+        rehm.init();
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("[DispatcherServlet] service started");
+        try {
+            Controller handler = rehm.findHandler(request.getRequestURI());
+            String viewName = handler.handleRequest(request, response);
+
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+            requestDispatcher.forward(request, response);
+
+        } catch (Exception e) {
+            log.error("exception occured: [{}]", e.getMessage(),e);
+            throw new ServletException(e);
+        }
+    }
+}
