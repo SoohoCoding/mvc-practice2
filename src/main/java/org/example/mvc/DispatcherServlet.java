66353d8a5cd1a341a6f75734441f0d161a515e49
soohocoding <ckdgns1447@gmail.com>
2023-11-13 15:03:44 +0900

    프런트 컨트롤러 실습 완료

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,42 +1,69 @@
 package org.example.mvc;
 
-import org.example.mvc.controller.Controller;
+import org.example.mvc.controller.RequestMethod;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.ModelAndView;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
 
-private static Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+    private List<HandlerMapping> handlerMappings;
 
-private RequestMappingHandlerMaping rehm;
+    private List<HandlerAdapter> handlerAdapters;
+
+    private List<ViewResolver> viewResolvers;
 
     @Override
-    public void init() throws ServletException {
-        rehm = new RequestMappingHandlerMaping();
-        rehm.init();
+    public void init() {
+        RequestMappingHandlerMapping rmhm = new RequestMappingHandlerMapping();
+        rmhm.init();
+
+        AnnotationHandlerMapping ahm = new AnnotationHandlerMapping("org.example");
+        ahm.initialize();
+
+        handlerMappings = List.of(rmhm, ahm);
+        handlerAdapters = List.of(new SimpleControllerHandlerAdapter(), new AnnotationHandlerAdapter());
+        viewResolvers = Collections.singletonList(new JspViewResolver());
     }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.info("[DispatcherServlet] service started");
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        String requestURI = request.getRequestURI();
+        RequestMethod requestMethod = RequestMethod.valueOf(request.getMethod());
+
+        Object handler = handlerMappings.stream()
+                .filter(hm -> hm.findHandler(new HandlerKey(requestURI, requestMethod)) != null)
+                .map(hm -> hm.findHandler(new HandlerKey(requestURI, requestMethod)))
+                .findFirst()
+                .orElseThrow(() -> new ServletException("No handler for [" + requestMethod + ", " + requestURI + "]"));
+
         try {
-            Controller handler = rehm.findHandler(request.getRequestURI());
-            String viewName = handler.handleRequest(request, response);
+            HandlerAdapter handlerAdapter = handlerAdapters.stream()
+                    .filter(ha -> ha.supports(handler))
+                    .findFirst()
+                    .orElseThrow(() -> new ServletException("No adapter for handler [" + handler + "]"));
 
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-            requestDispatcher.forward(request, response);
+            ModelAndView modelAndView = handlerAdapter.handle(request, response, handler);
 
-        } catch (Exception e) {
-            log.error("exception occured: [{}]", e.getMessage(),e);
+            for (ViewResolver viewResolver : this.viewResolvers) {
+                View view = viewResolver.resolveViewName(modelAndView.getViewName());
+                view.render(modelAndView.getModel(), request, response);
+            }
+        } catch (Throwable e) {
+            logger.error("exception occurred: [{}]", e.getMessage(), e);
             throw new ServletException(e);
         }
     }
